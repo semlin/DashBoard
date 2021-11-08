be2b0c9ffcc13372eafdac7094f177af6adc8799
semlin <valentin.aubert.45@gmail.com>
2021-11-08 20:41:34 +0100

    Ajout JS
    Ajout JS
    Some changes

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-var a = 2;
+var a = 3;
 console.log(a);
+
+
+function changeColor() {
+  console.log(document.getElementById('di1').classList);
+  console.log(document.getElementById('di1').classList.item(1));
+  var b = document.getElementById('di1').classList.item(1)
+  console.log("Je log b");
+  console.log(b);
+
+  document.getElementById("di1").classList.replace(b, "bg-warning")
+  console.log(document.getElementById('di1').classList);
+}
+changeColor()
