ec24f8fd910197846d284d6ef5e7982cd4fd238e
semlin <valentin.aubert.45@gmail.com>
2021-11-08 23:08:10 +0100

    update
    update structure
    update js

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
+
 var a = 3;
+
+//Nombre de case du dashboard a refresh
+var divAupdt = 9;
+
+//Compteur d'iteration
+var i = 0;
+
+//Random pour tests
+var rdm = 3;
+
 console.log(a);
 
-
-function changeColor() {
-  console.log(document.getElementById('di1').classList);
-  console.log(document.getElementById('di1').classList.item(1));
-  var b = document.getElementById('di1').classList.item(1)
-  console.log("Je log b");
-  console.log(b);
-
-  document.getElementById("di1").classList.replace(b, "bg-warning")
-  console.log(document.getElementById('di1').classList);
+//Fonction pour avoir des nombres randoms
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
-changeColor()
+
+//Fonction qui color la case en fction d'un random
+function changeColor(z) {
+  var b = document.getElementById(z).classList.item(1)
+  rdm = getRandomInt(3);
+
+  if ( rdm == 0){
+    document.getElementById(z).classList.replace(b, "bg-success")
+  }
+  else if ( rdm == 1){
+    document.getElementById(z).classList.replace(b, "bg-warning")
+  }
+  else if ( rdm == 2){
+    document.getElementById(z).classList.replace(b, "bg-danger")
+  }
+}
+
+
+function updateDiv(){
+  while (i != divAupdt){
+    i++
+    let z = 'di' + i
+      changeColor(z)
+  }
+
+
+}
+
+
+
+
+updateDiv()
